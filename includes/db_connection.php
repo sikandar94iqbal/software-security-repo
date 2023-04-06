6e5f5314ca74f11b59327ccd4b3960abcdc26dfc
Sikandar iqbal <32866763+sikandar94iqbal@users.noreply.github.com>
2023-04-05 21:36:05 -0300

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 // DB connection
-$con = mysqli_connect("localhost", "root", "", "Demo_projects");
+$con = mysqli_connect("3.85.170.231:3306", "root", "my-secret-pw", "software_security");
 if (!$con) { echo "server not connected"; }
 if($con){ }
 else{ echo "Failed to Connectt";}
